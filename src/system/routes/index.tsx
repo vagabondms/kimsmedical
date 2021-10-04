f9bfa72aa1b84445c356f3e3689aa3391be4be82
vagabondms <hiseokseok@gmail.com>
2021-10-04 20:01:15 +0900

    feat(nav): nav에 hover bar 생성
    margin과 left 가 핵심

---
+++ src/system/routes/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Examinations from '@containers/Examinations';
+import Information from '@containers/Information';
+import Introduce from '@containers/Introduce';
+import Main from '@containers/Main';
+import Map from '@containers/Map';
+import Notice from '@containers/Notice';
+import Reservation from '@containers/Reservation';
+import Tour from '@containers/Tour';
+import path from '@system/constants/paths';
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path={path.main.path}>
+        <Main />
+      </Route>
+      <Route path={path.introduce.path}>
+        <Introduce />
+      </Route>
+      <Route path={path.information.path}>
+        <Information />
+      </Route>
+      <Route path={path.reservation.path}>
+        <Reservation />
+      </Route>
+      <Route path={path.notice.path}>
+        <Notice />
+      </Route>
+      <Route path={path.tour.path}>
+        <Tour />
+      </Route>
+      <Route path={path.map.path}>
+        <Map />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routes;
